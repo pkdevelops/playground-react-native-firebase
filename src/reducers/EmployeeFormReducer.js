058429b83ebe7d3b9de7a036a78a2af796bcd6b9
Pradeep <pradeepdevelops@gmail.com>
2018-01-04 23:05:36 -0600

    end of course

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,5 +1,8 @@
 import {
-	EMPLOYEE_UPDATE
+	EMPLOYEE_UPDATE,
+	EMPLOYEE_CREATE,
+	EMPLOYEES_FETCH_SUCCESS,
+	EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types'
 
 const INITIAL_STATE = {
@@ -10,8 +13,12 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case EMPLOYEE_CREATE:
+			return INITIAL_STATE
 		case EMPLOYEE_UPDATE:
 			return { ...state, [action.payload.prop]: action.payload.value }
+		case EMPLOYEE_SAVE_SUCCESS:
+			return INITIAL_STATE
 		default:
 			return state
 	}
